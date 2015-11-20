6e1eb84e71d62e61b62bc20c41fdf8a538011ddd
Sathvik Swamy <sathvik_1@hotmail.com>
2015-11-20 01:42:20 -0500

    Updated item profile fragment with better UI and a custom number picker.

--- app/src/main/java/com/productions/jalebi/ItemViewFragment.java
+++ app/src/main/java/com/productions/jalebi/ItemViewFragment.java
@@ -2,6 +2,7 @@ package com.productions.jalebi;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -33,7 +35,7 @@ import org.w3c.dom.Text;
  * create an instance of this fragment.
  */
 
-public class ItemViewFragment extends Fragment {
+public class ItemViewFragment extends Fragment implements View.OnClickListener {
 
     private static Item selectedItem;
     private OnFragmentInteractionListener mListener;
@@ -84,7 +86,44 @@ public class ItemViewFragment extends Fragment {
             descTextView.setText(selectedItem.getDescription());
         }
 
+        // ------------ Custom Number Picker ---------------
+        // Set default value of zero for the order value
+        TextView orderValue = (TextView) v.findViewById(R.id.item_view_order_value);
+        orderValue.setText("0");
+        // Get controls
+        Button incrementButton = (Button) v.findViewById(R.id.item_view_order_increment);
+        Button decrementButton = (Button) v.findViewById(R.id.item_view_order_decrement);
+        // Register onClickListeners:
+        incrementButton.setOnClickListener(this);
+        decrementButton.setOnClickListener(this);
+
         return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView orderValue;
+        int numericValue;
+
+        switch(v.getId()) {
+            case R.id.item_view_order_decrement:
+                // Increment the value held by the TextView
+                orderValue = (TextView) getActivity().findViewById(R.id.item_view_order_value);
+                numericValue = Integer.parseInt(orderValue.getText().toString());
+                numericValue++;
+                orderValue.setText(""+numericValue);
+                break;
+
+            case R.id.item_view_order_increment:
+                // Decrement the value held by the TextView (only if it is > 0)
+                orderValue = (TextView) getActivity().findViewById(R.id.item_view_order_value);
+                numericValue = Integer.parseInt(orderValue.getText().toString());
+                if (numericValue > 0) {
+                    numericValue--;
+                }
+                orderValue.setText(""+numericValue);
+                break;
+        }
     }
 
     @Override
